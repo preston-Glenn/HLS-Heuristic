0542609200f2601982c36a8e0165507da6efdc20
Preston Glenn <preston.glenn@outlook.com>
2021-10-20 01:26:26 -0500

    initial commit

---
+++ HLS_Heuristic.cpp
@@ -0,0 +1,81 @@
+// HLS_Heuristic.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+
+
+
+
+
+#include <tclap\CmdLine.h>
+#include <string>
+#include <iostream>
+#include <algorithm>
+
+
+
+using namespace TCLAP;
+using namespace std;
+
+
+
+
+int main(int argc, char** argv) {
+	// Wrap everything in a try block.  Do this every time,
+	// because exceptions will be thrown for problems.
+	try {
+		// Define the command line object.
+		CmdLine cmd("Command description message", ' ', "0.9");
+
+		// Define a value argument and add it to the command line.
+		ValueArg<string> nameArg("n", "name", "Name to print", true, "homer",
+			"string");
+		cmd.add(nameArg);
+
+		// Define a switch and add it to the command line.
+		SwitchArg reverseSwitch("r", "reverse", "Print name backwards", false);
+		cmd.add(reverseSwitch);
+
+		// Parse the args.
+		cmd.parse(argc, argv);
+
+		// Get the value parsed by each arg.
+		string name = nameArg.getValue();
+		bool reverseName = reverseSwitch.getValue();
+
+		// Do what you intend too...
+		if (reverseName) {
+			reverse(name.begin(), name.end());
+			cout << "My name (spelled backwards) is: " << name << endl;
+		}
+		else
+			cout << "My name is: " << name << endl;
+
+	}
+	catch (ArgException& e)  // catch any exceptions
+	{
+		cerr << "error: " << e.error() << " for arg " << e.argId() << endl;
+	}
+
+
+
+}
+
+
+
+
+void bruteForce(string**lists,string* newList,int length,int count = 0) {
+
+	for (int i = 0; i < length; i++) {
+
+		
+		if (count == length) {
+			//append last element @[i]
+			//synthesis
+		}
+		else {
+			bruteForce(lists, newList.append(lists[count][i].count + 1)
+		}
+	}
+
+
+}
