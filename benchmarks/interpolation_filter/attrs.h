ce130617dd66f0cce7bbca789b038a1e0b8fb154
preston-Glenn <preston.glenn@outlook.com>
2021-11-05 01:08:18 -0500

    ran interpolation

---
+++ benchmarks/interpolation_filter/attrs.h
@@ -0,0 +1,5 @@
+#define ATTR1 Cyber unroll_times=0
+#define ATTR2 Cyber unroll_times=all
+#define ATTR3 Cyber unroll_times=0
+#define ATTR4 Cyber unroll_times=all
+#define ATTR5 Cyber unroll_times=2
