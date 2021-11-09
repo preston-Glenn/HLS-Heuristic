26e92a8398ca37039c5da9bd4b556f3e864ea817
preston-Glenn <preston.glenn@outlook.com>
2021-11-09 16:26:40 -0600

    ran final

--- benchmarks/ave16/attrs.h
+++ benchmarks/ave16/attrs.h
@@ -1,3 +1,3 @@
 #define ATTR1 Cyber array=REG
-#define ATTR2 Cyber unroll_times=0
-#define ATTR3 Cyber unroll_times=4
+#define ATTR2 Cyber unroll_times=all
+#define ATTR3 Cyber unroll_times=0
--- benchmarks/interpolation_filter/attrs.h
+++ benchmarks/interpolation_filter/attrs.h
@@ -1,5 +1,5 @@
 #define ATTR1 Cyber unroll_times=4
-#define ATTR2 Cyber unroll_times=2
+#define ATTR2 Cyber unroll_times=4
 #define ATTR3 Cyber unroll_times=4
 #define ATTR4 Cyber unroll_times=4
 #define ATTR5 Cyber unroll_times=4
